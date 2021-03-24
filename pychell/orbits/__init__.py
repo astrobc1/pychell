610cb0d8b4193a0ab197ecab644597c9f13f7d12
Bryson <bryson.cale1@gmail.com>
2021-03-24 12:49:28 -0400

    Trying to fix numpy errrs

--- pychell/orbits/__init__.py
+++ pychell/orbits/__init__.py
@@ -1,1 +1,15 @@
 name = 'orbits'
+
+# optimize
+from optimize.knowledge import *
+from optimize.optimizers import *
+from optimize.samplers import *
+
+# pychell
+from pychell.orbits.rvdata import *
+from pychell.orbits.rvmodels import *
+from pychell.orbits.rvmodels import *
+from pychell.orbits.rvkernels import *
+from pychell.orbits.rvscores import *
+from pychell.orbits.rvprob import *
+from pychell.orbits.gls import *
