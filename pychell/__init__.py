56de39395e5f65779dd2e9390ad80aafaca0b08d
Bryson Cale <bryson.cale1@gmail.com>
2022-04-04 12:29:33 -0700

    Redux updates for ishell

--- pychell/__init__.py
+++ pychell/__init__.py
@@ -1,12 +1,2 @@
-name = 'reduce'
-
-from .deconv2d import *
-from .optimal import *
-#from .decomp import *
-
-from .extract import *
-from .precalib import *
-from .recipe import ReduceRecipe
-from .trace import *
-
-from .sregion import SpecRegion2d
+name = 'pychell'
+__version__ = "1.1.0"
